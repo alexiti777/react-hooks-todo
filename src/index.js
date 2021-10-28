235704bc2e65ba3ae4528de5d68933c90326e68f
alexiti777 <90395600+alexiti777@users.noreply.github.com>
2021-10-28 15:54:33 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Typography from '@material-ui/core/Typography';
-import './styles.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import TodoList from "./TodoList";
+import TodoForm from "./TodoForm";
+import Typography from "@material-ui/core/Typography";
+import useTodoState from "./useTodoState";
+import "./styles.css";
 
 const App = () => {
+  const { todos, addTodo, deleteTodo } = useTodoState([]);
   return (
     <div className="App">
       <Typography component="h1" variant="h2">
         Todos
       </Typography>
+
+      <TodoForm
+        saveTodo={(todoText) => {
+          const trimmedText = todoText.trim();
+
+          if (trimmedText.length > 0) {
+            addTodo(trimmedText);
+          }
+        }}
+      />
+
+      <TodoList todos={todos} deleteTodo={deleteTodo} />
     </div>
   );
 };
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
