480ef52d907dffdd77d99a71a0c3c862eca89c51
alexiti777 <90395600+alexiti777@users.noreply.github.com>
2021-10-28 14:31:46 +0300

    2

--- docs/src/index.js
+++ docs/src/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
+import React, { useState } from 'react';
 import Typography from '@material-ui/core/Typography';
 import './styles.css';
 
